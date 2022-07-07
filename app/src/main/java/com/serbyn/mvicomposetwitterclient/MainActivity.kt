3d354cc091de1b10364ec52389d7a69c57a5f4e8
Vitalii Serbyn <vivitseser@gmail.com>
2022-07-07 23:56:41 +0300

    Update Main Activity

--- app/src/main/java/com/serbyn/mvicomposetwitterclient/MainActivity.kt
+++ app/src/main/java/com/serbyn/mvicomposetwitterclient/MainActivity.kt
@@ -3,13 +3,9 @@ package com.serbyn.mvicomposetwitterclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.insets.ProvideWindowInsets
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.serbyn.mvicomposetwitterclient.databinding.ContentMainBinding
 import com.serbyn.mvicomposetwitterclient.ui.theme.MviComposeTwitterClientTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,27 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MviComposeTwitterClientTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                ProvideWindowInsets(consumeWindowInsets = false) {
+                    AndroidViewBinding(ContentMainBinding::inflate)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MviComposeTwitterClientTheme {
-        Greeting("Android")
     }
 }
